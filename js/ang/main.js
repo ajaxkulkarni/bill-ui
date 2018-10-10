a72c702c499b83569516d64184406924fdf3775f
ajaxkulkarni <ajinkyashiva@gmail.com>
2018-10-10 19:32:18 +0530

    payment related changes

--- js/ang/main.js
+++ js/ang/main.js
@@ -5,7 +5,7 @@ app.config(['cfpLoadingBarProvider', function (cfpLoadingBarProvider) {
   }]);
 
 //var host = "http://localhost:8080/billapp-service";
-var host = "http://173.255.197.204:8080/billapp";
+var host = "https://payperbill.in:8443/billapp";
 var root = host + "/user/";
 //var projectRoot = host + "/projectService"
 //var rootAdmin = host + "/adminService";
